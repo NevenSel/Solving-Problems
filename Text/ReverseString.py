5fff9dcdccf955662c6d95440021f0577cde14e8
NevenSel <NevenSelimovic@gmail.com>
2014-10-11 18:51:02 +0900

    Reverse a String
    Enter a string and the program will reverse it and print it out.

---
+++ Text/ReverseString.py
@@ -0,0 +1,13 @@
+##Reverse a string
+
+print "Hello, this will reverse any string of characters you input. Give it a try!"
+
+some_string = str(raw_input("Please enter a string "))
+
+print "Your string was: ", some_string
+print "Reversed, it is: ", some_string[::-1]
+    
+
+
+    
+    
